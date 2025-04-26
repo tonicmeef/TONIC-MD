a59f9086a48cf9e07e440caeee33e70f321a3a9b
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-04-26 08:49:30 +0200

    Update main-prefix.js

--- plugins/main-prefix.js
+++ plugins/main-prefix.js
@@ -2,7 +2,7 @@ const { cmd, commands } = require('../command');
 const config = require('../config');
 const prefix = config.PREFIX;
 const fs = require('fs');
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions2');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions');
 const { writeFileSync } = require('fs');
 const path = require('path');
 
