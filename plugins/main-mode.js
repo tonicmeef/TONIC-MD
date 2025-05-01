0750a12fcdaee5e9375edf57a9319929be723aff
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-05-01 13:29:31 +0200

    Update main-mode.js

--- plugins/main-mode.js
+++ plugins/main-mode.js
@@ -2,19 +2,24 @@ const { cmd, commands } = require('../command');
 const config = require('../config');
 const prefix = config.PREFIX;
 const fs = require('fs');
+const axios = require('axios')
+const os = require("os")
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions');
 const { writeFileSync } = require('fs');
-const path = require('path'); 
+const path = require('path');
+
+
+let antilinkAction = "off"; // Default state
+let warnCount = {}; // Track warnings per user
+
 
 cmd({
     pattern: "mode",
-    alias: ["setmode"],
-    react: "🛠",
     desc: "Set bot mode to private or public.",
     category: "settings",
     filename: __filename,
-}, async (conn, mek, m, { from, args, isCreator, reply }) => {
-    if (!isCreator) return reply("*📛 Only the owner can use this command!*");
+}, async (conn, mek, m, { from, args, isOwner, reply }) => {
+    if (!isOwner) return reply("*📛 Only the owner can use this command!*");
 
     // Si aucun argument n'est fourni, afficher le mode actuel et l'usage
     if (!args[0]) {
@@ -25,10 +30,15 @@ cmd({
 
     if (modeArg === "private") {
         config.MODE = "private";
-        return reply("✅ Bot mode is now set to *PRIVATE*.");
+        return reply("*_BOT MODE IS NOW SET TO PRIVATE ✅_*.");
     } else if (modeArg === "public") {
         config.MODE = "public";
-        return reply("✅ Bot mode is now set to *PUBLIC*.");
+        return reply("*_BOT MODE IS NOW SET TO PUBLIC ✅_*.")
+        const {exec} = require("child_process")
+reply("*_DATABASE UPDATE 𝗧𝗢𝗡𝗜𝗖-𝐌𝐃 RESTARTING NOW...🚀_*")
+await sleep(1500)
+exec("pm2 restart all")
+reply("*_𝗧𝗢𝗡𝗜𝗖-𝐌𝐃 STARTED NOW...🚀_*");
     } else {
         return reply("❌ Invalid mode. Please use `.mode private` or `.mode public`.");
     }
