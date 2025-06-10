25fcb5158579f60bc4b343e190d30382b339c119
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-06-11 00:59:35 +0200

    Update tts-trt.js

--- plugins/tts-trt.js
+++ plugins/tts-trt.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *JAWADTECHX-KHANX-TRANSLATION*
+        const translationMessage = `> *TONIC-MD-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
