9535bb79fe9cff48f0dda9dd64ffca77e835c5cf
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-06-11 00:38:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -73,7 +73,7 @@ DEV: process.env.DEV || "263783866754",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
